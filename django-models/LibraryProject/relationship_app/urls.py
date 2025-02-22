3628b1c5e2a0827e82f40dc6080a9c552f44717b
Tilbo239 <roaimtilbo6363@gmail.com>
2025-02-22 17:15:57 +0100

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from .views import register_view, login_view, logout_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', list_books, name='book_list'),  # For the function-based view
     path('', views.LibraryDetailView.as_view(), name='library_detail'),  # For the class-based view
+    path('', register_view, name='register'),
+    path('', login_view, name='login'),
+    path('', logout_view, name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
@@ -22,6 +24,31 @@ class LibraryDetailView(DetailView):
         context['books'] = self.object.books.all()
         return context
 
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')  # Remplace 'home' par ton URL d'accueil
+    else:
+        form = UserCreationForm()
+    return render(request, 'auth/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('list_books')  # Remplace 'home' par ton URL d'accueil
+    else:
+        form = AuthenticationForm()
+    return render(request, 'auth/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 
     # template_name = 'relationship_app/library_detail.html'
     # context_object_name = 'library'
