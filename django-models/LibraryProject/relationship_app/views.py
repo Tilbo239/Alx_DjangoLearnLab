d073f91083dcb6e64f7e8066b6ac9e733aac64db
Tilbo239 <roaimtilbo6363@gmail.com>
2025-02-28 14:31:43 +0100

    Admin view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Book
+from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.detail import DetailView
 from .models import Library
 
@@ -51,13 +52,10 @@ def logout_view(request):
     return redirect('login')
 
 
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
 
-
-
-
-
-    # template_name = 'relationship_app/library_detail.html'
-    # context_object_name = 'library'
-
-    # def get_queryset(self):
-    #     return Library.objects.all()
+# Vue Admin
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
