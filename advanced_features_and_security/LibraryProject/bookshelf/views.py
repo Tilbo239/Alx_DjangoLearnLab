24292bbe3e26dd7b10f902b948416579b28f0fc1
Tilbo239 <roaimtilbo6363@gmail.com>
2025-03-06 18:43:10 +0100

    custom permissions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponse
 
-# Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def comment_list(request):
+   
+     return HttpResponse("You can view the books!")
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_comment(request):
+    return HttpResponse("You can create a comment!")
+    
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_comment(request, comment_id):
+    return HttpResponse("Post updated!")
+    
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_comment(request, comment_id):
+    return HttpResponse("Post deleted!") 
