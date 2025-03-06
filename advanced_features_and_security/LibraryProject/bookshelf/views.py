11451ae81fc733bcf09ec7ce1d768fac2e018f00
Tilbo239 <roaimtilbo6363@gmail.com>
2025-03-06 18:46:38 +0100

    custom permissions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -6,22 +6,3 @@ from django.http import HttpResponse
 def book_list(request):
    
      return HttpResponse("Book list!")
-
-# @permission_required('bookshelf.can_view', raise_exception=True)
-# def post_list(request):
-   
-#      return HttpResponse("Post list!")
-
-# @permission_required('bookshelf.can_create', raise_exception=True)
-# def create_post(request):
-#     return HttpResponse("Post created!")
-    
-
-# @permission_required('bookshelf.can_edit', raise_exception=True)
-# def edit_post(request, article_id):
-#     return HttpResponse("Post updated!")
-    
-
-# @permission_required('bookshelf.can_delete', raise_exception=True)
-# def delete_post(request, article_id):
-#     return HttpResponse("Post deleted!") 
