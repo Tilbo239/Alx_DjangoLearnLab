6154f8dd0600363790b46798771b081ed6acce86
Tilbo239 <roaimtilbo6363@gmail.com>
2025-03-07 11:48:17 +0100

    import ExamplrForm in a views.py file

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.decorators import permission_required
 from django.views.decorators.http import require_http_methods
 from django.http import HttpResponse
+from .forms import ExampleForm
 
 @require_http_methods(["GET"])
 @permission_required('bookshelf.can_view', raise_exception=True)
