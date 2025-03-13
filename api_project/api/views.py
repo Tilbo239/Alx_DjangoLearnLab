ec8158e97a67a4db548915be2605ecd285383457
Tilbo239 <roaimtilbo6363@gmail.com>
2025-03-13 14:13:06 +0100

    advanced_api_project project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -9,9 +9,22 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+
+"""
+This module defines views for the Book model using Django REST framework.
+Classes:
+    BookViewSet: A viewset that provides CRUD operations for Book instances.
+Authentication and Permissions:
+    - BookViewSet: This viewset enforces authentication and permissions:
+        - IsAuthenticated: Ensures that the user is authenticated.
+        - IsAdminUser: Ensures that the user is an admin.
+    Users must be both authenticated and have admin privileges to perform any operations (create, read, update, delete) on the Book instances through this viewset.
+"""
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated, IsAdminUser]
+
+
     
 
