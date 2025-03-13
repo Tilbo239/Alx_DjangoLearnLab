99617340c48008ee2917590b78ef443cd2899c32
Tilbo239 <roaimtilbo6363@gmail.com>
2025-03-13 15:25:12 +0100

    configure the project et models definitions

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
 class Author(models.Model):
+
+    """
+    Model representing an author.
+    An author can have multiple books.
+    """
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
+    """
+    Model representing a book.
+    Each book is linked to an author via a ForeignKey relationship.
+    """
     title = models.CharField(max_length=100)
     publication_date = models.IntegerField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} ({self.publication_year})"
