e1024178e1505b5eeee6a1023c283bb7493981bd
Tilbo239 <roaimtilbo6363@gmail.com>
2025-03-13 12:50:54 +0100

    Generate and Use Tokens, Define Permission Classes

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics 
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 from .models import Book
 from .serializers import BookSerializer
@@ -11,4 +12,6 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    
 
