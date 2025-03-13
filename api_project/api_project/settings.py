23738150b221699fbee1da3885244fbfd09be1cf
Tilbo239 <roaimtilbo6363@gmail.com>
2025-03-13 12:15:37 +0100

    Configure Authentication

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -27,6 +27,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ],
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10
+}
+
 
 # Application definition
 
@@ -39,8 +50,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
 
-    'api'
+    'api',
+
 ]
 
 MIDDLEWARE = [
